b51204403a482e95405e857490fd64523ebea6b4
nayyara-airlangga <nayyara.airlangga@gmail.com>
2022-02-06 00:25:48 +0700

    Install dependencies and configure next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
+const withReactSvg = require("next-react-svg")
+const path = require("path")
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withReactSvg({
   reactStrictMode: true,
-}
+  include: path.resolve(__dirname, "public/assets"),
+  webpack(config) {
+    return config
+  },
+})
 
 module.exports = nextConfig
