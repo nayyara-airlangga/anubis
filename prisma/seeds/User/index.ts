0bd73d5123c499fad57a49b7bd6132c5947c302a
nayyara-airlangga <nayyara.airlangga@gmail.com>
2022-02-07 22:22:24 +0700

    Install bcrypt, use bcrypt for password seeding

--- prisma/seeds/User/index.ts
+++ prisma/seeds/User/index.ts
@@ -1,28 +1,32 @@
+import bcrypt from "bcrypt"
+
+const salt = bcrypt.genSaltSync(10)
+
 const UserSeed = [
   {
     email: "example@example.com",
     name: "John Doe Mike",
     username: "johndoe",
-    password: "anjay132",
+    password: bcrypt.hashSync("anjay123", salt),
   },
   {
     email: "example2@example.com",
     name: "John Doe James",
     username: "johndoe2",
-    password: "anjay231",
+    password: bcrypt.hashSync("anjay321", salt),
   },
   {
     email: "example3@example.com",
     name: "John Doe Jack",
     username: "johndoe3",
-    password: "anjay321",
+    password: bcrypt.hashSync("anjay231", salt),
   },
   {
     email: "example4@example.com",
     name: "John Doe Jim",
     username: "johndoe4",
     bio: "I am a bio",
-    password: "anjay123",
+    password: bcrypt.hashSync("anjay132", salt),
   },
 ]
 
