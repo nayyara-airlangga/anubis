1bf649246690843c24d7c1682a47855bd78a73b4
nayyara-airlangga <nayyara-airlangga@gmail.com>
2022-03-07 20:09:20 +0700

    Create auth context and hook

---
+++ app/constants/enums/loadstatus/index.ts
@@ -0,0 +1,7 @@
+const enum LoadStatus {
+  LOADING = "LOADING",
+  SUCCESS = "SUCCESS",
+  ERROR = "ERROR",
+}
+
+export { LoadStatus }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
 import type { AppProps } from "next/app"
 
+import { AuthProvider } from "@contexts"
+
 import "@styles/globals.css"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  )
 }
 
 export default MyApp
